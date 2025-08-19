b4dd763658a3f7c53c322331e13731c48b009eae
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-08-19 13:40:04 +0000

    [Finish] 4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def top
   end
 
@@ -8,13 +10,13 @@ class UsersController < ApplicationController
 end
 
   def edit
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(user.id)
+    redirect_to user_path(@user.id)
 end
 
 private
@@ -22,5 +24,14 @@ private
 def user_params
   params.require(:user).permit(:name, :profile_image)
 end
+
+def is_matching_login_user
+  user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to post_images_path
+  end
+end
+
+
 end
 
